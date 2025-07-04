0bf2d6c6c4c455d27e9c091373e040aaab066bb7
Jordan Hill <jordan.hill@comcast.net>
2025-07-04 00:47:14 -0700

    Add ICCore Submodule

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <Window.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    ICcore::Window window(1280, 720, "Demo");
+
+    while (!window.ShouldClose()) {
+        window.ProcessInput();
+        window.SwapBuffers();
+    }
     return 0;
 }
